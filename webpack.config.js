05c6d605bd04cf5ba1b2f32fe6497016d84d2b0a
Petri Kivikangas <petri.kivikangas@protonmail.com>
2016-08-16 17:27:50 +0900

    Use NamedModulesPlugin in development mode

--- webpack.config.js
+++ webpack.config.js
@@ -9,16 +9,14 @@ const MODE_DEV_SERVER = process.argv[1].indexOf('webpack-dev-server') > -1 ? tru
 const FAIL_ON_ERROR = process.env.FAIL_ON_ERROR ? JSON.parse(process.env.FAIL_ON_ERROR) : !MODE_DEV_SERVER; // disabled on dev-server mode, enabled in build mode
 const OPTIMIZE = process.env.OPTIMIZE ? JSON.parse(process.env.OPTIMIZE) : NODE_ENV === 'production';
 
-const plugins = [];
-
-plugins.push(
+const plugins = [
 	// Necessary for applying the correct environment everywhere
 	new Webpack.DefinePlugin({
 		'process.env': {
 			'NODE_ENV': JSON.stringify(NODE_ENV)
 		}
 	})
-);
+];
 
 if (!FAIL_ON_ERROR) {
 	plugins.push(new Webpack.NoErrorsPlugin());
@@ -57,6 +55,7 @@ const conf = {
 
 // webpack-dev-server mode
 if (MODE_DEV_SERVER) {
+	plugins.push(new Webpack.NamedModulesPlugin());
 	plugins.push(new Webpack.HotModuleReplacementPlugin());
 	conf.PORT = DEV_SERVER_PORT;
 	conf.devtool = 'eval';
@@ -65,11 +64,11 @@ if (MODE_DEV_SERVER) {
 		'react-hot-loader/patch', // this has to be the first loaded plugin in order to work properly!
 		'webpack-dev-server/client?http://0.0.0.0:' + DEV_SERVER_PORT, // WebpackDevServer host and port
 		'webpack/hot/only-dev-server', // 'only' prevents reload on syntax errors
-		'./src/app.jsx' // appʼs entry point
+		'./src/app' // appʼs entry point
 	];
 } else {
 	conf.devtool = 'source-map';
-	conf.entry = ['babel-regenerator-runtime', './src/app.jsx'];
+	conf.entry = ['babel-regenerator-runtime', './src/app'];
 }
 
 module.exports = conf;
