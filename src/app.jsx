55f73e24a4f60b793adba7cdb376d309ed53e859
Petri Kivikangas <petri.kivikangas@protonmail.com>
2016-09-20 22:25:33 +0900

    reformat

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,17 @@
 // React
 import React from 'react';
-import {render} from 'react-dom';
-import {browserHistory} from 'react-router';
-import {AppContainer} from 'react-hot-loader';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router';
+import { AppContainer } from 'react-hot-loader';
 
 // Redux
-import {applyMiddleware, compose, createStore} from 'redux';
-import {syncHistoryWithStore} from 'react-router-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { syncHistoryWithStore } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import {rootReducer, rootSaga} from './reducers';
+import { rootReducer, rootSaga } from './reducers';
 import Root from './components/root';
 
 function configureStore(initial_state, middleware) {
