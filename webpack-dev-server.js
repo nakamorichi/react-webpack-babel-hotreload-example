cdaf572349f9855c236267fd1b6b81adc865e6fd
Petri Kivikangas <petri.kivikangas@protonmail.com>
2016-11-26 13:59:03 +0900

    Minor refactoring

--- webpack-dev-server.js
+++ webpack-dev-server.js
@@ -9,19 +9,19 @@ const WebpackConfig = require('./webpack.config');
 const DEV_SERVER_PORT = process.env.PORT;
 
 new WebpackDevServer(Webpack(WebpackConfig), {
-	publicPath: WebpackConfig.output.publicPath,
-	hot: true,
+	contentBase: Path.resolve(__dirname, 'public'),
 	historyApiFallback: true,
-	contentBase: Path.join(__dirname, 'public'),
+	hot: true,
+	publicPath: WebpackConfig.output.publicPath,
 	stats: {
 		colors: true,
 		chunks: false
 	}
-}).listen(DEV_SERVER_PORT, 'localhost', function (err, result) {
+}).listen(DEV_SERVER_PORT, 'localhost', (err, result) => {
 	if (err) {
 		return console.log(err);
 	}
 
-	console.log('Listening at http://localhost:' + DEV_SERVER_PORT + '/');
-	console.log('NODE_ENV=[' + process.env.NODE_ENV + ']');
+	console.log(`[${process.pid}] Webpack development server running at http://localhost:${DEV_SERVER_PORT}`);
+	console.log(`[${process.pid}] NODE_ENV=[${process.env.NODE_ENV}]`);
 });
