01e13c17db1aefd2fc566d2e0707791ee8a9e657
Petri Kivikangas <petri.kivikangas@protonmail.com>
2017-01-14 19:23:28 +0900

    Simplify Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,12 @@ const Webpack = require('webpack');
 
 const pkg = require('./package.json');
 
+const TARGET = process.env.npm_lifecycle_event;
 const DEV_SERVER_PORT = process.env.PORT = process.env.PORT || 7000;
 const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
-const MODE_DEV_SERVER = process.argv[1].indexOf('webpack-dev-server') > -1 ? true : false;
-const FAIL_ON_ERROR = process.env.FAIL_ON_ERROR ? JSON.parse(process.env.FAIL_ON_ERROR) : !MODE_DEV_SERVER; // disabled on dev-server mode, enabled in build mode
-const OPTIMIZE = process.env.OPTIMIZE ? JSON.parse(process.env.OPTIMIZE) : NODE_ENV === 'production';
+const IN_DEV_MODE = NODE_ENV == 'development';
 
+// common plugins
 const plugins = [
 	new Webpack.DefinePlugin({
 		'process.env': {
@@ -29,18 +29,7 @@ const plugins = [
 	})
 ];
 
-if (!FAIL_ON_ERROR) {
-	plugins.push(new Webpack.NoErrorsPlugin());
-}
-
-if (OPTIMIZE) {
-	plugins.push(new Webpack.optimize.UglifyJsPlugin({
-		compressor: {
-			warnings: false
-		}
-	}));
-}
-
+// common configuration
 const conf = {
 	output: {
 		path: Path.join(__dirname, 'public/assets'),
@@ -66,13 +55,14 @@ const conf = {
 	plugins: plugins
 };
 
-// webpack-dev-server mode
-if (MODE_DEV_SERVER) {
+if (IN_DEV_MODE) { // development mode (webpack-dev-server)
+	plugins.push(new Webpack.NoErrorsPlugin());
 	plugins.push(new Webpack.NamedModulesPlugin());
 	plugins.push(new Webpack.HotModuleReplacementPlugin());
 	plugins.push(new Webpack.SourceMapDevToolPlugin({
 		filename: '[file].map'
 	}));
+
 	conf.performance = {
 		hints: false
 	};
@@ -87,17 +77,24 @@ if (MODE_DEV_SERVER) {
 			Path.join(__dirname, 'src/app') // appʼs entry point
 		]
 	};
-} else {
+} else { // production mode (bundling)
+	plugins.push(new Webpack.optimize.UglifyJsPlugin({
+		compressor: {
+			warnings: false
+		}
+	}));
+
 	plugins.push(new CleanWebpackPlugin(['public/assets'], {
 		root: Path.resolve(__dirname),
 		verbose: true,
 		dry: false,
 		exclude: ['.gitignore']
 	}));
+
 	conf.output.filename = '[name]-[chunkhash].js';
 	conf.entry = {
 		main: Path.join(__dirname, 'src/app'),
-		vendor: Object.keys(pkg.dependencies)
+		vendor: Object.keys(pkg.dependencies) // determine vendor contents from package.json
 	};
 }
 
